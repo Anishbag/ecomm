f58f2df4a6714b8a06ca5b5dfcf8eedfb8fdf5c8
Anish Bag <anishbag18@gmail.com>
2026-09-23 19:10:38 +0530

    cros

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, MinLength, IsString } from "class-validator";
+
+export class LoginDto {
+    @IsEmail()
+    email:string;
+
+    @IsString()
+    @MinLength(6)
+    password: string;
+
+}
+    
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
-import { log } from 'console';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
